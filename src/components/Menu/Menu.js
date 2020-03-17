bfa1bf850e588563af10556e68cabc1167483cb3
Karen Nguyen <knguyen4184@yahoo.com>
2020-03-16 21:34:36 -0600

    Menu creates button html

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,4 +3,17 @@ import './Menu.css'
 
 // MENU COMPONENT CODE GOES HERE
 
+const Menu = () => {
+
+  return (
+    <div>
+      <button name='local' >Local</button>
+      <button name='entertainment' >Entertainment</button>
+      <button name='health' >Health</button>
+      <button name='science' >Science</button>
+      <button name='technology' >Technology</button>
+    </div>
+  );
+}
+
 export default Menu;
