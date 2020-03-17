63cee166ee900a0dc91b488dd8714e3aac5dea2b
Karen Nguyen <knguyen4184@yahoo.com>
2020-03-16 21:33:56 -0600

    Add menu to App class

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,15 +4,19 @@ import entertainment from '../../data/entertainment';
 import health from '../../data/health';
 import science from '../../data/science';
 import technology from '../../data/technology'
-
 import './App.css';
 import NewsContainer from '../NewsContainer/NewsContainer';
+import Menu from '../Menu/Menu';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      local,
+      entertainment,
+      health,
+      science,
+      technology,
     }
   }
 
@@ -20,6 +24,7 @@ class App extends Component {
     return (
       <div className="app">
         <NewsContainer localNews={local}></NewsContainer>
+        <Menu></Menu>
       </div>
     );
   }
