8a26bfe461992b925e3d3b49b0432598bb718ef5
Karen Nguyen <knguyen4184@yahoo.com>
2020-03-16 19:49:45 -0600

    Add sizes to div container on news article

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -4,7 +4,7 @@ import './NewsArticle.css';
 // NEWSARTICLE COMPONENT CODE GOES HERE
 
 const NewsArticle = (props) => {
-  return <div>
+  return <div className='news-article-div'>
     <h2>{props.news.headline}</h2>
     <img className='article-image' src={props.news.img} />
     <p>{props.news.description}</p>
