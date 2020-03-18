2a5455428de9913d0dd765df5fa091e849453ae1
Karen Nguyen <knguyen4184@yahoo.com>
2020-03-17 21:37:14 -0600

    Added comment

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -24,6 +24,7 @@ class App extends Component {
   switchPage = (page) => {
     this.setState({currentPage: this.state[page]});
   }
+// everytime you setState Render runs
 
   render () {
     return (
