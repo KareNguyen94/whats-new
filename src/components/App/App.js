7e7a1397eda12a9febdfa908b7df4c794ad527b4
Karen Nguyen <knguyen4184@yahoo.com>
2020-03-18 11:07:02 -0600

    Render seach form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,6 +7,7 @@ import technology from '../../data/technology'
 import './App.css';
 import NewsContainer from '../NewsContainer/NewsContainer';
 import Menu from '../Menu/Menu';
+import SearchForm from '../SearchForm/SearchForm'
 
 class App extends Component {
   constructor() {
@@ -29,7 +30,9 @@ class App extends Component {
   render () {
     return (
       <div className="app">
-        <h1 className='header'>What's New</h1>
+        <h1 className='header'>What's New
+        <SearchForm></SearchForm>
+        </h1>
         <div className='nav'> 
           <Menu className='nav' switchPage={this.switchPage}></Menu>
         </div>
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,6 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './SearchForm.css';
 
 // SEARCHFORM COMPONENT CODE GOES HERE
+
+class SearchForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+ 
+
+  render() {
+    return (
+      <form>
+        <input 
+          type="text" 
+          placeholder="Search for news article here"
+          value={this.state.input}
+        >
+        </input>
+
+        <button
+          type="button"
+
+        >Search
+        </button>
+      </form>
+    )
+  }
+}
 
 export default SearchForm;
